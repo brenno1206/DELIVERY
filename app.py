50ec601ce60ceb9bd25aaf960b2c7dde2a30a803
Brenno <151473774+brenno1206@users.noreply.github.com>
2026-04-28 11:46:59 -0300

    Enhance create_app with logging and test config support
    Added logging configuration and test config handling to create_app function.

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
+import logging
 from flask import Flask
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
+
+    if app.debug:
+        app.logger.setLevel(logging.DEBUG)
 
     from delivery.ext.config import init_app as init_config
     init_config(app)
+    
+    if test_config:
+        app.config.update(test_config)
 
-    from delivery.ext.wtf import init_app as init_wtf
-    init_wtf(app)
+    if not app.config.get("TESTING"):
+        from delivery.ext.wtf import init_app as init_wtf
+        init_wtf(app)
 
     from delivery.ext.debugtoolbar import init_app as init_toolbar
     init_toolbar(app)
 
-    from delivery.views import init_app as init_main
-    init_main(app)
-
+    from delivery.views import init_app as init_site
+    init_site(app)
+    
     return app
-
 
 # py .\make_env.py
 # .\venv\Scripts\activate
